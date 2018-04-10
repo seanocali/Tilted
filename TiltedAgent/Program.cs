0056a56b408b5d4d61e5da45fd3ae5708ae2a096
Sean O'Neil <sean.oneil78@hotmail.com>
2018-04-09 19:02:30 -0700

    Add project files.

---
+++ TiltedAgent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using TiltedAgent.ViewModels;
+using TiltedAgent.Views;
+
+namespace TiltedAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
